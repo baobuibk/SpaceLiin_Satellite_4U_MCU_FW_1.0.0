de6b8737ac994f8a74a99c9f6d7dba2a3881349f
baobuibk <buiqbao@gmail.com>
2026-02-06 13:47:33 +0700

    feat:add i2c driver

--- Dev/M3_Driver/components/i2c/i2c_io.h
+++ Dev/M3_Driver/components/i2c/i2c_io.h
@@ -18,6 +18,7 @@ typedef struct i2c_io_t{
 }i2c_io_t;
 
 uint32_t i2c_io_init(i2c_io_t *me);
+uint32_t i2c_io_enable(i2c_io_t *me);
 uint32_t i2c_io_send(struct i2c_io_t *me, uint8_t ui8SlaveAddr, const char *buf, int count);
 uint32_t i2c_io_recv(struct i2c_io_t *client, uint8_t ui8SlaveAddr,  char *buf, int count);
 
